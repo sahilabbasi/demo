a8aae3c7e5e6630b84e0f9c7aa7a8ad3f3b38a35
sahilabbasi <79418362+sahilabbasi@users.noreply.github.com>
2022-01-17 20:06:38 +0530

    Add files via upload

--- test.php
+++ test.php
@@ -1,20 +1,67 @@
+<!-- LFI vulernbility !-->
+
+
 <?php
 
-$test = "data:text/html;base64,PHNjcmlwdD5hbGVydCgiWFNTIik7PC9zY3JpcHQ+Cg==";
+// local fle inclusion stuck on here
+
+$x  = $_GET["page"];
 
 
+$pattern = "/sam/i";
+
+$lfi = preg_match($pattern,$x);
 
 
-header("location:$test");
+// $lfi = str_replace("","_",$x);
 
-$input = " w'h'o'am'i ";
-
-$x = escapeshellcmd($input);
-
- system($x);
-
-
+if($lfi == 1)
+{
+       http_response_code(403);
+        die("You're Blocked :(");
+    }
+    else
+    {
+   $x1 = fopen($x,"r");
+   echo fread($x1,100000);
+}
 
 
 
 ?>
+
+<!-- csrf vulernbility !-->
+
+<!-- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+// $x = hash("sha256",rand()*100);
+
+
+// setcookie("csrf",$x, time() + (86400 * 30),"/");
+
+?>
+
+    <form action="<?php ?>" method="get">
+    <input type="hidden" name="csrf" value="<?php  ?>">
+    Enter You're Name: <input type="text" name="name" id="name">
+    <button>Submit</button>
+    
+    </form>
+</body>
+</html>
+
+<?php
+
+// echo $x;
+
+?> -->
